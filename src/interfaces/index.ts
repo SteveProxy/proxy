7cfd66376f688da1c8943af969bcac451c509cc3
Пронин Егор <mrzillagold@gmail.com>
2021-03-13 22:30:03 +1000

    Fix interfaces path

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -4,7 +4,8 @@ export * from "./Utils";
 export * from "./proxy/Client";
 export * from "./proxy/Proxy";
 export * from "./proxy/modules/Context";
-export * from "./proxy/modules/PagesBuilder";
+export * from "./proxy/modules/pagesBuilder/PagesBuilder";
+export * from "./proxy/modules/chatManager/ChatManager";
 export * from "./proxy/modules/PluginManager";
 
 export * from "./proxy/plugins/Spotify";
