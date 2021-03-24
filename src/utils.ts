1e5673604001c4f19d17c6ef8fa3b89c8d257f9b
Пронин Егор <mrzillagold@gmail.com>
2021-03-25 03:52:17 +1000

    Improve parseIP function

--- src/utils.ts
+++ src/utils.ts
@@ -22,18 +22,31 @@ export function isValidNickname(nickname: string): boolean {
 }
 
 export function parseIP(ip: string): IParsedIP {
+    const LOCALHOST = "127.0.0.1";
+
     const parsedIP = ip.match(/([^]+):([\d]+)/);
 
     if (parsedIP) {
         return {
-            host: parsedIP[1],
+            host: parsedIP[1] !== "0.0.0.0" ?
+                parsedIP[1]
+                :
+                LOCALHOST,
             port: Number(parsedIP[2])
         };
     }
 
+    const isPort = ip.startsWith(":");
+
     return {
-        host: ip || "127.0.0.1",
-        port: 25565
+        host: ip && !isPort && ip !== "0.0.0.0" ?
+            ip
+            :
+            LOCALHOST,
+        port: isPort ?
+            Number(ip.slice(1))
+            :
+            25565
     };
 }
 
