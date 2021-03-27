eea5387b8d4db82bef3375c098d7ca88be2785f2
Пронин Егор <mrzillagold@gmail.com>
2021-03-27 20:20:03 +1000

    Improve Discord RPC plugin

--- src/proxy/plugins/Discord/Discord.ts
+++ src/proxy/plugins/Discord/Discord.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord-rpc";
+import { Client, Presence } from "discord-rpc";
 
 import { Plugin } from "../Plugin";
 import { Proxy } from "../../Proxy";
@@ -6,6 +6,9 @@ import { Proxy } from "../../Proxy";
 export class Discord extends Plugin {
 
     private client: Client = new Client({ transport: "ipc" });
+    private activity: Presence;
+    private reconnectAttempts = 3;
+    private activityUpdatesInterval?: NodeJS.Timeout;
 
     constructor(proxy: Proxy) {
         super(proxy, {
@@ -13,34 +16,78 @@ export class Discord extends Plugin {
             description: "Discord интеграция",
             prefix: "§9§lDiscord"
         });
+
+        const { proxy: { version } } = this.proxy.config;
+
+        this.activity = {
+            largeImageKey: "minecraft",
+            largeImageText: `Minecraft ${version}`,
+            smallImageKey: "steve",
+            smallImageText: `SteveProxy | ${this.proxy.client.username}`,
+            startTimestamp: Date.now()
+        };
     }
 
     start(): void {
         const client = this.client;
-        const { proxy: { version }, lobby: { host, port }, plugins: { discord } } = this.proxy.config;
 
         client.on("ready", () => {
-            client.setActivity({
-                largeImageKey: "minecraft",
-                largeImageText: `Minecraft ${version}`,
-                smallImageKey: "steve",
-                smallImageText: `SteveProxy | ${this.proxy.client.username}`,
-                details: `${host}:${port}`,
-                startTimestamp: Date.now()
-            });
-
             this.proxy.client.context.send(`${this.meta.prefix} Авторизован под ${client.user.username}.`);
+
+            this.startActivityUpdates();
         });
 
-        client.login(discord)
-            .catch((error) => {
-                this.proxy.client.context.send(`${this.meta.prefix} §cПроизошла ошибка при авторизации!`);
+        this.login();
+    }
 
+    private login(): void {
+        const { plugins: { discord } } = this.proxy.config;
+
+        this.client.login(discord)
+            .catch((error) => {
                 console.error(error);
+
+                if (this.reconnectAttempts) {
+                    this.reconnectAttempts--;
+
+                    return this.login();
+                }
+
+                this.proxy.client.context.send(`${this.meta.prefix} §cПроизошла ошибка при авторизации!`);
             });
+    }
+
+    private startActivityUpdates() {
+        this.activityUpdatesInterval = setInterval(() => {
+            const details = this.proxy.currentServer === this.proxy.fallbackServer ?
+                "В лобби"
+                :
+                this.proxy.currentServer;
+
+            this.setActivity({
+                details
+            });
+        }, 1_000);
+    }
+
+    private setActivity(activity: Presence) {
+        const updatedActivity = {
+            ...this.activity,
+            ...activity
+        };
+
+        if (JSON.stringify(this.activity) !== JSON.stringify(updatedActivity)) {
+            this.activity = updatedActivity;
+
+            this.client.setActivity(updatedActivity);
+        }
     }
 
     stop(): void {
         this.client.destroy();
+
+        if (this.activityUpdatesInterval) {
+            clearInterval(this.activityUpdatesInterval);
+        }
     }
 }
