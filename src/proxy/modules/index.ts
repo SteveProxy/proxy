a4fb5b067d9731aba0eb32010f75b538c345dcba
Пронин Егор <mrzillagold@gmail.com>
2021-06-05 22:33:13 +1000

    fix(modules): change export position

--- src/proxy/modules/index.ts
+++ src/proxy/modules/index.ts
@@ -1,8 +1,8 @@
+export * from "./chatManager/ChatManager";
+export * from "./packetManager/PacketManager";
+export * from "./pagesBuilder/PagesBuilder";
+
 export * from "./QuestionBuilder";
 export * from "./PluginManager";
 export * from "./Context";
 export * from "./API";
-
-export * from "./chatManager/ChatManager";
-export * from "./packetManager/PacketManager";
-export * from "./pagesBuilder/PagesBuilder";
