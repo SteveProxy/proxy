35417f0657b43f4e2945f1ca8145c8780f4ac909
Pronin Egor <mrzillagold@gmail.com>
2022-01-05 12:14:09 +1000

    chore(lobby): use logic packet const

--- src/lobby.ts
+++ src/lobby.ts
@@ -2,35 +2,17 @@ import { createServer } from 'minecraft-protocol';
 
 import { config } from './config';
 
-import { getVersion, minecraftData } from './utils';
+import { getVersion, LOBBY_LOGIN_PACKET } from './utils';
 
-const { proxy, lobby: _lobby, bridge: { title } } = config;
+const { proxy, lobby: _lobby, bridge: { title } } = config.data!;
 
 const lobby = createServer({
     ...proxy,
     ..._lobby
 });
 
-// @ts-ignore Invalid lib types
-const loginPacket = minecraftData.loginPacket;
-
 lobby.on('login', (client) => {
-    loginPacket.entityId = client.id;
-    loginPacket.isHardcore = true;
-    loginPacket.gameMode = 0;
-    loginPacket.dimension.value.infiniburn.value = 'minecraft:infiniburn_end';
-    loginPacket.dimension.value.effects.value = 'minecraft:the_end';
-
-    client.write('login', loginPacket);
-
-    const channel = client.protocolVersion >= getVersion('1.13-pre3') ?
-        'brand'
-        :
-        'MC|Brand';
-
-    client.registerChannel(channel, ['string', []]);
-    client.writeChannel(channel, title);
-
+    client.write('login', LOBBY_LOGIN_PACKET);
     client.write('position', {
         x: 0,
         y: 64,
@@ -39,4 +21,12 @@ lobby.on('login', (client) => {
         pitch: 0,
         flags: 0x00
     });
+
+    const channel = client.protocolVersion >= getVersion('1.13-pre3') ?
+        'brand'
+        :
+        'MC|Brand';
+
+    client.registerChannel(channel, ['string', []]);
+    client.writeChannel(channel, title);
 });
