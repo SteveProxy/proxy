ec39c462f5d2a1a89387b330b7ff0d6e313ef906
Pronin Egor <mrzillagold@Gmail.com>
2022-05-15 21:37:10 +1000

    chore(config): always update current protocol version

--- src/config.ts
+++ src/config.ts
@@ -32,7 +32,7 @@ config.data ||= {
         port: 25565,
         maxPlayers: 1,
         version: '1.18.2',
-        protocolVersion: getVersion('1.18.2'),
+        protocolVersion: 0,
         motd: '                       §6§lSteveProxy',
         favicon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAIAAAAlC+aJAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABlUlEQVRoge2awUoCQRiAd93R1cIsiPQg1Bv0AuExgo517yDUoUeIOkfRpUuXnqCIDnWoDh669QoFliWVBxFEk9XV7QW+OXj6Gfi/4zfrjJ8Dswuuv7qS94h4gtozfgp9NI55nniMPpXiedImQJ8k/H14FofQAGk0QBoNkMbYznsbccIfmM1m0BcyPvqxx+d9uxfx9YnlfoLWITRAGg2QRgOkMbaB0/0d9GGaz/tcdg79oN/lBQL+7Tq/3+iPbx/QO78DGiCNBkijAdL4V4e7ODAThuj/In5eDwK+vtPtoF8uLqFvfLyit+H8DmiANBogjQZIY2zn/ebBOfqtygn6aqWO3nbeX9wX0d88n6G/PtpD7/wOaIA0GiCNBkjjX1bXcaBYKqM34Tz6u1ptqoW3N9bQNz/f0NdbbfTO74AGSKMB0miANKY34v9fh01+vm989dHnU+mpFn58ekFfKufQ+x7P7/wOaIA0GiCNBkhj3n9aODCZ8HtBiwuFqRaIRkP0uTCLvjfg+5JtHud3QAOk0QBpNECaf0Y5Vy/cCoDAAAAAAElFTkSuQmCC',
         description: [
@@ -57,9 +57,7 @@ config.data ||= {
     }
 };
 
-if (!config.data.proxy.protocolVersion) {
-    config.data.proxy.protocolVersion = getVersion(config.data.proxy.version);
-}
+config.data.proxy.protocolVersion = getVersion(config.data.proxy.version);
 
 await config.write();
 
